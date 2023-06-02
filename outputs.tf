dd61804b65d6a70b7d06efd29e2094e75a97e43d
wongcheehao <120695430+wongcheehao@users.noreply.github.com>
2023-06-02 19:43:04 +0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-output "instance_ami" {
-  value = aws_instance.ubuntu.ami
-}
-
-output "instance_arn" {
-  value = aws_instance.ubuntu.arn
-}
-
